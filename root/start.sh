b330a44ed5ef46b10000930825ec33ad5595b8f0
666wcy <769020367@qq.com>
2021-02-05 19:25:40 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -2,4 +2,4 @@
 sudo /usr/sbin/service cron start
 chmod 0777 /upload/ -R
 python3 /upload/config.py
-yes "" | qbittorrent-nox --webui-port=$PORT
+yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config
